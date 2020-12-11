ecf8b89b382e81bace4d79a6e13f9590830c5f05
free-ko <47565280+free-ko@users.noreply.github.com>
2020-12-11 16:20:10 +0900

    수정
    - 요구한 URL로 크롤링 가능

--- epicmobile/crawling/main.py
+++ epicmobile/crawling/main.py
@@ -4,60 +4,44 @@ from bs4 import BeautifulSoup
 from urllib import parse
 from datetime import datetime
 import time
-import threading
 
 def crawling() :
-    # 기본 페이지에 변수를 설정합니다.
-    base_url = 'https://www.coupang.com/np/search?q={}&page={}'
-    keyword = input("검색할 키워드 : ")
+    base_url = 'https://www.coupang.com/np/categories/417869/?page={}'
     headers = {
         'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'    
     }
     result_list = []
-    url = base_url.format(keyword,1)
-    print(url)
+    url = base_url.format(1)
     res = requests.get(url, headers=headers)
     if res.status_code == 200:
         soup = BeautifulSoup(res.text)
-        print(soup)
-
-        # 마지막 페이지를 지정합니다.
-        last_page = soup.select_one('a.btn-last').text.strip()
-        print(last_page)
 
 
-    # 에러를 확인 하기 위해 error_cnt를 작성합니다.
+
     error_cnt = 0
-
-    # 마지막 페이지까지 for문을 통해 크롤링을 작동합니다.
-    for page in range(1, int(last_page)+1):
-        url = base_url.format(keyword, page)
+    for page in range(1, 18):
+        url = base_url.format(page)
+        print(url)
         res = requests.get(url, headers=headers)
         if res.status_code == 200:
             soup = BeautifulSoup(res.text)
-            item_list = soup.select('ul#productList li a')
+            item_list = soup.select('ul#productList li a dl dt.image')
             for item in item_list:
                 try:
-                    link = item.select_one('img.search-product-wrap-img').get('src')
-                    link = link.replace('//','')    # 가져온 url 중 불필요한 부분과 파일을 제거 합니다.
-                    if '.gif' in link:              
+                    link = item.select_one('img').get('src')
+                    link = link.replace('//','')
+                    if '.gif' in link:
                         continue
                     result_list.append([link])
                 except:
                     error_cnt += 1
 
 
-    # 크롤링 한 결과를 csv파일로 저장 합니다.
-    # 현재 날짜를 기입하도록 설정 합니다.
-    # pandas를 통해 2차원 파일로 저장합니다.
+    # 크롤링 내역 저장
     curr = datetime.now().strftime('%Y-%m-%d')
-    filename = '쿠팡조회결과_{}_{}.csv'.format(keyword,curr)
+    filename = '쿠팡조회결과_{}.csv'.format(curr)
     df = pd.DataFrame(result_list, columns=['link'])
     df.to_csv(filename, index=False, encoding='utf-8')
     print('fail to save :', error_cnt)
-
-    # 테스트로 3초 마다 함수 실행
-    # 하루는 86400초
-    threading.Timer(3, crawling).start()
 
 crawling()
--- epicmobile/crawling/테스트 코드/test.py
+++ epicmobile/crawling/테스트 코드/test.py
@@ -4,41 +4,60 @@ from bs4 import BeautifulSoup
 from urllib import parse
 from datetime import datetime
 import time
+import threading
 
-base_url = 'https://www.coupang.com/np/categories/417869/?page={}'
-headers = {
-    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'    
-}
-result_list = []
-url = base_url.format(1)
-print(url)
-res = requests.get(url, headers=headers)
-if res.status_code == 200:
-    soup = BeautifulSoup(res.text)
-    print(soup)
-    last_page = soup.select_one(".selected").text.strip()
-    print(last_page)
-
-
-error_cnt = 0
-cp_url = 'https://www.coupang.com/417869/'
-for page in range(1, int(last_page)+1):
-    url = base_url.format(page)
+def crawling() :
+    # 기본 페이지에 변수를 설정합니다.
+    base_url = 'https://www.coupang.com/np/search?q={}&page={}'
+    keyword = input("검색할 키워드 : ")
+    headers = {
+        'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'    
+    }
+    result_list = []
+    url = base_url.format(keyword,1)
+    print(url)
     res = requests.get(url, headers=headers)
     if res.status_code == 200:
         soup = BeautifulSoup(res.text)
-        item_list = soup.select('ul#productList li dl.baby-product-wrap dt.image img')
-        for item in item_list:
-            try:
-                images = item.select_one("src")
-                result_list.append(images)
-            except:
-                error_cnt += 1
+        print(soup)
+
+        # 마지막 페이지를 지정합니다.
+        last_page = soup.select_one('a.btn-last').text.strip()
+        print(last_page)
 
 
-# 크롤링 내역 저장
-curr = datetime.now().strftime('%Y-%m-%d')
-filename = '쿠팡조회결과_{}_{}.csv'.format(curr)
-df = pd.DataFrame(result_list, columns=['title','link'])
-df.to_csv(filename, index=False, encoding='utf-8')
-print('fail to save :', error_cnt)
+    # 에러를 확인 하기 위해 error_cnt를 작성합니다.
+    error_cnt = 0
+
+    # 마지막 페이지까지 for문을 통해 크롤링을 작동합니다.
+    for page in range(1, int(last_page)+1):
+        url = base_url.format(keyword, page)
+        res = requests.get(url, headers=headers)
+        if res.status_code == 200:
+            soup = BeautifulSoup(res.text)
+            item_list = soup.select('ul#productList li a')
+            for item in item_list:
+                try:
+                    link = item.select_one('img.search-product-wrap-img').get('src')
+                    link = link.replace('//','')    # 가져온 url 중 불필요한 부분과 파일을 제거 합니다.
+                    if '.gif' in link:              
+                        continue
+                    result_list.append([link])
+                except:
+                    error_cnt += 1
+
+
+    # 크롤링 한 결과를 csv파일로 저장 합니다.
+    # 현재 날짜를 기입하도록 설정 합니다.
+    # pandas를 통해 2차원 파일로 저장합니다.
+    curr = datetime.now().strftime('%Y-%m-%d')
+    filename = '쿠팡조회결과_{}_{}.csv'.format(keyword,curr)
+    df = pd.DataFrame(result_list, columns=['link'])
+    df.to_csv(filename, index=False, encoding='utf-8')
+    print('fail to save :', error_cnt)
+
+    # 테스트로 3초 마다 함수 실행
+    # 하루는 86400초
+    threading.Timer(3, crawling).start()
+
+crawling()
